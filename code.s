b0c254582f67f38fc33e535aecab054f157448e3
StippMan <ra102919@uem.br>
2021-10-21 08:49:34 -0300

    Criei o arquivo

---
+++ code.s
@@ -0,0 +1,27 @@
+/*	To do:
+	( ) 1 - Leitura dos Conjuntos
+	( ) 2 - Encontrar União
+	( ) 3 - Encontrar Intersecção
+	( ) 4 - Encontrar a Diferença
+	( ) 5 - Encontrar o Complementar
+*/
+
+.section .data
+
+
+// Data section
+
+
+.section .text
+.globl	_start
+
+_start:
+
+
+// Code here
+
+
+_end:
+
+	pushl	$0
+	call	exit
